e9361690994e8bb89d2852def7131d4972199e7c
DarrenWu <deirenwu1101@gmail.com>
2024-02-11 02:26:16 +0800

    Update telescope picker type

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,16 @@ return {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+      require('telescope').setup {
+        pickers = {
+          find_files = {
+            theme = "dropdown",
+          },
+          live_grep = {
+            theme = "ivy",
+          },
+        },
+      }
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
