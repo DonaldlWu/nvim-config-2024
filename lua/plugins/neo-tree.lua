9707582a8761779f9d3dd3ae7ffaa78e17bf365f
DarrenWu <deirenwu1101@gmail.com>
2024-02-11 01:21:59 +0800

    Update neo-tree setting

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,29 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>')
-  end
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      source_selector = {
+        winbar = true,
+        content_layout = "center",
+      },
+      filesystem = {
+        follow_current_file = true,
+        hijack_netrw_behavior = "open_current",
+        use_libuv_file_watcher = true,
+        window = { mappings = { h = "toggle_hidden" } },
+      },
+      event_handlers = {
+        {
+          event = "neo_tree_buffer_enter",
+          handler = function(_)
+            vim.opt_local.signcolumn = "auto"
+          end,
+        },
+      },
+    })
+
+    vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>")
+    vim.keymap.set("n", "<leader>o", "<cmd>Neotree focus<cr>")
+  end,
 }
