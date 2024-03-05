727039ba8b2ea9d2abdafcb0284bd75560e94666
DarrenWu <deirenwu1101@gmail.com>
2024-03-06 00:55:26 +0800

    Add rest for api client test

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript", "typescript"},
+      ensure_installed = {"lua", "javascript", "typescript", "http", "json"},
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
