14bd72bb4356b9d307f0a5807e2f5f959405e45b
DarrenWu <deirenwu1101@gmail.com>
2024-03-07 02:49:08 +0800

    Add telescope plugin for file browser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,45 +1,74 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({
-				pickers = {
-					find_files = {
-						theme = "dropdown",
-					},
-					live_grep = {
-						theme = "ivy",
-					},
-					lsp_references = {
-						theme = "ivy",
-					},
-				},
-			})
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>rf", builtin.lsp_references, {})
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      {
+        -- For the current buffer, open the file browser
+        "sf",
+        function()
+          local telescope = require("telescope")
 
-			-- extensions
-			-- aerial
-			vim.keymap.set("n", "<leader>6", function()
-				require("telescope").extensions.aerial.aerial()
-			end, { noremap = true, silent = true })
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+          local function telescope_buffer_dir()
+            return vim.fn.expand("%:p:h")
+          end
+
+          telescope.extensions.file_browser.file_browser({
+            path = "%:p:h",
+            cwd = telescope_buffer_dir(),
+            respect_gitignore = false,
+            hidden = true,
+            grouped = true,
+            previewer = false,
+            initial_mode = "normal",
+            layout_config = { height = 40 },
+          })
+        end,
+        desc = "Open File Browser with the path of the current buffer",
+      },
+    },
+    config = function()
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            theme = "dropdown",
+          },
+          live_grep = {
+            theme = "ivy",
+          },
+          lsp_references = {
+            theme = "ivy",
+          },
+        },
+      })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>rf", builtin.lsp_references, {})
+
+      -- extensions
+      -- aerial
+      vim.keymap.set("n", "<leader>6", function()
+        require("telescope").extensions.aerial.aerial()
+      end, { noremap = true, silent = true })
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
