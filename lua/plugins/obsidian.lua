0a688b8598ef249f2e62543322fb4112c0cfee34
DarrenWu <deirenwu1101@gmail.com>
2024-03-08 02:03:20 +0800

    Add obsidian plugin

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,56 @@
+return {
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("obsidian").setup({
+        workspaces = {
+          {
+            name = "ObsidianVault",
+            path = "/Users/wuderen/Documents/Obsidian Vault",
+          },
+          {
+            name = "Notion-Export",
+            path = "/Users/wuderen/WorkSpace/NotionExport/Notion/Notion-Export",
+          },
+        },
+        mappings = {
+          -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+          ["gf"] = {
+            action = function()
+              return require("obsidian").util.gf_passthrough()
+            end,
+            opts = { noremap = false, expr = true, buffer = true },
+          },
+          -- Toggle check-boxes.
+          ["<leader>ch"] = {
+            action = function()
+              return require("obsidian").util.toggle_checkbox()
+            end,
+            opts = { buffer = true },
+          },
+        },
+        templates = {
+          subdir = "Templates",
+          date_format = "%Y-%m-%d",
+          time_format = "%H:%M:%S",
+          tags = "",
+          substitutions = {
+            yesterday = function()
+              return os.date("%Y-%m-%d", os.time() - 86400)
+            end,
+            tomorrow = function()
+              return os.date("%Y-%m-%d", os.time() + 86400)
+            end,
+          },
+        },
+      })
+      vim.opt.conceallevel = 2
+    end,
+  },
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript", "typescript", "http", "json"},
+      ensure_installed = {"lua", "javascript", "typescript", "http", "json", "markdown"},
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
